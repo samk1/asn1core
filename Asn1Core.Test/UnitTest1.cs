bfd243c436f7194cc656977a7b2687af1e6548f3
Sam Kidman <samuel@netwealth.com.au>
2018-11-03 12:47:16 +1100

    initial commit

---
+++ Asn1Core.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Asn1Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
